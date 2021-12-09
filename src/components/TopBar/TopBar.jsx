9c90f29f67eb3f03e0419d1031df1dc0c95c8e43
vladimir greenfield <vladdygreenfield@gmail.com>
2021-12-09 14:35:58 -0500

    created the entire navigation bar with search bar and icons

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,4 +1,7 @@
 import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import "./TopBar.css";
+
+
 
 function TopBar() {
     return(
@@ -8,14 +11,16 @@ function TopBar() {
             </div>
             <div className="topbarCenter">
                 <div className="searchBar">
-                    <Search />
+                    <Search className="searchIcon" />
                     <div placeholder="Search for friends or posts" className="searchImput"></div>
                 </div>
             </div>
             <div className="topbarRight">
                 <div className="topbarLinks">
-                    <span className="topbarLink">Homepage</span>
-                    <span className="topbarLink">Timeline</span>
+                    <span className="topbarLink">Home</span>
+                    <span className="topbarLink">Friends</span>
+                    <span className="topbarLink">Posts</span>
+                    <span className="topbarLink">Logout</span>
                 </div>
                 <div className="topbarIcons">
                     <div className="topbarIconItem">
